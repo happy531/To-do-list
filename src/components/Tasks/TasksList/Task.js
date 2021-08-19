5a04d7df006789f767e2b77d285429ba83ee4c4e
happy531 <jedrzejwesolowski177@gmail.com>
2021-08-19 23:02:47 +0200

    replace delete button with options button

--- src/components/Tasks/TasksList/Task.js
+++ src/components/Tasks/TasksList/Task.js
@@ -1,25 +1,48 @@
+import { Fragment, useState } from "react";
 import TaskDoneButton from "../../UI/Buttons/TaskDoneButton";
-import TaskRemoveButton from "../../UI/Buttons/TaskRemoveButton";
+import TaskOptionsButton from "../../UI/Buttons/TaskOptionsButton";
+import Card from "../../UI/Card/Card";
 
 import classes from "./Task.module.css";
 
 const Task = (props) => {
+  const [showOptionsButton, setShowOptionsButton] = useState(false);
+  const [showOptions, setShowOptions] = useState(false);
+
   const removeTaskHandler = () => {
     props.onRemoveTask(props.id);
   };
   const compleateTaskHandler = () => {
     props.onCompleateTask(props.id);
   };
+  const showOptionsHandler = () => {
+    setShowOptions((prev) => !prev);
+  };
   return (
-    <li>
-      <div className={classes["accept-btn"]}>
-        <TaskDoneButton onClick={compleateTaskHandler} />
-      </div>
-      <label className={classes["task-text"]}>{props.text}</label>
-      <div className={classes["delete-btn"]}>
-        <TaskRemoveButton onClick={removeTaskHandler} />
-      </div>
-    </li>
+    <Fragment>
+      <li
+        onMouseEnter={() => setShowOptionsButton(true)}
+        onMouseLeave={() =>
+          showOptions ? setShowOptionsButton(true) : setShowOptionsButton(false)
+        }
+      >
+        <div>
+          <TaskDoneButton onClick={compleateTaskHandler} />
+        </div>
+        <label className={classes["task-text"]}>{props.text}</label>
+        <div>
+          {showOptionsButton && (
+            <TaskOptionsButton onClick={showOptionsHandler} />
+          )}
+        </div>
+        {showOptions && (
+          <Card className={classes.menu}>
+            <button onClick={removeTaskHandler}>Delete</button>
+            <button>Sub task</button>
+          </Card>
+        )}
+      </li>
+    </Fragment>
   );
 };
 
